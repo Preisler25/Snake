66c840eacac598e0a2538e5aef17223e58361857
Preisler <preisler.andras@gmail.com>
2023-04-06 23:14:13 +0200

    main

---
+++ src/main/java/preislerdev/javasnaketest/MenuController.java
@@ -0,0 +1,19 @@
+package preislerdev.javasnaketest;
+
+import javafx.fxml.FXML;
+
+public class MenuController {
+    @FXML
+    protected void toGamePage() {
+        System.out.println("toGamePage");
+    }
+    @FXML
+    protected void toSettingsPage() {
+        System.out.println("toSettingsPage");
+    }
+    @FXML
+    protected void toProfilePage() {
+        System.out.println("toProfilePage");
+    }
+
+}
