d2efbb30033954b0059340b70222ed6a3338e52f
Preisler <preisler.andras@gmail.com>
2023-04-07 20:26:32 +0200

    wokring

--- src/main/java/preislerdev/javasnaketest/FxmlFunc.java
+++ src/main/java/preislerdev/javasnaketest/FxmlFunc.java
@@ -13,11 +13,11 @@ public class FxmlFunc {
 
     public void loadPage(Parent root, ActionEvent event) {
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root, 1280, 720);
+        scene = new Scene(root, 1920, 1000);
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         stage.setScene(scene);
-        stage.setX(40);
-        stage.setY(40);
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("");
         stage.show();
     }
 
